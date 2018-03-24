185519e12bafc110a8e9a7c0c6119d49cbc84f86
Marco <costa.1691388@studenti.uniroma1.it>
2018-03-24 13:54:37 +0100

    Updated script to calculate total time for images detection

--- scripts/totalTime_darknet_voc.py
+++ scripts/totalTime_darknet_voc.py
@@ -8,7 +8,9 @@ def isfloat(value):
         return False
 
 #calculate total time        
-output = open("../output/darknet/darknet_voc.txt","r",encoding="utf-8")
+output = open("../output/darknet/darknet_voc2007.txt","r",encoding="utf-8")
+output.readline()
+output.readline() #First and second lines are not important
 for riga in output:
     words=riga.strip().split()
     for word in words:
@@ -18,7 +20,7 @@ for riga in output:
 output.close()
 
 #write total time on output file
-output = open("../output/darknet/darknet_voc.txt","a",encoding="utf-8")
+output = open("../output/darknet/darknet_voc2007.txt","a",encoding="utf-8")
 result = "\nTime(Image detection): "+str(totalTime)+" seconds ("+str(totalTime/60)+" minutes).\n"
 print(result, end="", file=output)
 output.close()
