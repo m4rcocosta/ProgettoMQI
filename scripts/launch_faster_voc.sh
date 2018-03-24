22027fa7ed68a9d09e3939f288243f70dfdc9f0f
Marco <costa.1691388@studenti.uniroma1.it>
2018-03-24 16:49:02 +0100

    Updated script to run faster-rcnn on voc2007

--- scripts/launch_faster_voc.sh
+++ scripts/launch_faster_voc.sh
@@ -1,3 +1,19 @@
+OUTPUT="output/faster/faster-rcnn_voc2007.txt"
+OUTPUT_CHECK="output/faster/faster-rcnn_voc2007_check.txt"
+
+#start
+start=$(date)
+start_seconds=$(date +%s)
+
+#clean files
+if [ -f ../$OUTPUT ]
+then rm ../$OUTPUT
+fi
+
+if [ -f ../$OUTPUT_CHECK ]
+then rm ../$OUTPUT_CHECK
+fi
+
 #move in faster-rcnn folder
 cd ../nets/py-faster-rcnn/tools
 
@@ -6,3 +22,34 @@ python faster-rcnn_voc.py
 
 #return in project's folder
 cd ../../..
+
+#results
+echo "____________________________________RESULTS____________________________________" >> $OUTPUT
+
+#end
+end=$(date)
+end_seconds=$(date +%s)
+
+total_time=$((end_seconds-start_seconds))
+
+total_time_minutes=$((total_time/60))
+remaining_seconds=$((total_time%60))
+total_time_hours=$((total_time_minutes/60))
+remaining_minutes=$((total_time_minutes%60))
+
+echo Start: $start >> $OUTPUT
+echo End: $end >> $OUTPUT
+
+#scripts directory
+cd scripts
+
+#calculate time in image detection
+python3 totalTime_faster_voc.py 
+
+echo "Total Time: $total_time seconds (${total_time_hours}h:${remaining_minutes}m:${remaining_seconds}s)." >> ../$OUTPUT
+
+#check
+python3 check_output_faster_voc.py
+
+#return in project's folder
+cd ..
