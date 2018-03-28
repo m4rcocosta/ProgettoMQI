85db66e931b2ae56b1618b03b593c7c2a668457b
Marco <costa.1691388@studenti.uniroma1.it>
2018-03-28 02:57:03 +0200

    Modified script start.sh

--- start.sh
+++ start.sh
@@ -2,16 +2,16 @@
 cd scripts
 
 #Run Darknet Yolo on Pascal VOC 2007 set
-bash launch_darknet_voc.sh
+bash launch_net.sh darknet voc2007 0.8
 
 #Run Faster-RCNN on Pascal VOC 2007 set
-bash launch_faster_voc.sh
+bash launch_net.sh faster-rcnn voc2007 0.8
 
 #Run Darknet Yolo COCO set
-bash launch_darknet_coco.sh
+bash launch_net.sh darknet coco2017 0.8
 
 #Run Faster-RCNN COCO set
-bash launch_faster_coco.sh
+bash launch_net.sh faster-rcnn coco2017 0.8
 
 #Return to project's directory
 cd ..
