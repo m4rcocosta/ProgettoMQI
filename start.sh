51e62ad3e795acd22d6441ea096b60d4ce95f2b4
Marco <costa.1691388@studenti.uniroma1.it>
2018-04-05 20:08:15 +0200

    Modified: Set treshold from terminal

--- start.sh
+++ start.sh
@@ -1,17 +1,27 @@
+args=("$@")
+if [ $# -ne 1 ]
+then {
+    echo "Error: Please insert correct parameters (TRESHOLD[0.0-1.0])"
+    exit 1
+}  
+fi
+
+TRESHOLD=${args[0]}
+
 #Move to scripts directory
 cd scripts
 
 #Run Darknet Yolo on Pascal VOC 2007 set
-bash launch_net.sh darknet voc2007 0.8
+bash launch_net.sh darknet voc2007 $TRESHOLD
 
 #Run Faster-RCNN on Pascal VOC 2007 set
-bash launch_net.sh faster-rcnn voc2007 0.8
+bash launch_net.sh faster-rcnn voc2007 $TRESHOLD
 
 #Run Darknet Yolo COCO set
-bash launch_net.sh darknet coco2017 0.8
+bash launch_net.sh darknet coco2017 $TRESHOLD
 
 #Run Faster-RCNN COCO set
-bash launch_net.sh faster-rcnn coco2017 0.8
+bash launch_net.sh faster-rcnn coco2017 $TRESHOLD
 
 #Return to project's directory
 cd ..
